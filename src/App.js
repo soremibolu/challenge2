eed5e6513d4b3df55b2a34cb9b0bf3558646dbda
soremibolu <soremibolu@gmail.com>
2022-09-09 09:06:27 +0100

    set up structure: adding pages, components and style folders and installing sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return <div className="App">smt</div>;
