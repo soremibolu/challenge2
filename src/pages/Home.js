be097fe1c9d07ea0e1f355fcc2a16014b11c036b
soremibolu <soremibolu@gmail.com>
2022-09-09 11:31:56 +0100

    set up filter for searched movies in Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
 import SearchPanel from "../components/SearchPanel";
 import Rail from "../components/Rail";
+import Data from "../data/data.json";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
-  const [search, setSearch] = useState({});
+  const [search, setSearch] = useState({
+    title: "",
+    director: "",
+    year: "",
+  });
+
+  const filterData = (MovieData, searchData) => {
+    const filteredTitles = MovieData.filter(
+      (movie) => movie.title === searchData.title
+    );
+    const filteredDirectors = MovieData.filter(
+      (movie) => movie.director === searchData.director
+    );
+    const filteredYears = MovieData.filter(
+      (movie) => movie.year === searchData.year
+    );
+
+    const moviesFiltered = filteredTitles.concat(
+      filteredDirectors,
+      filteredYears
+    );
+
+    console.log(moviesFiltered);
+  };
 
   const handleSearch = (title, director, year) => {
     setSearch({
@@ -12,7 +36,9 @@ const Home = () => {
       director: director ? director : "",
       year: year ? year : "",
     });
+    filterData(Data.movies, search);
   };
+  //console.log(MovieData.movies);
 
   //all
   //random
